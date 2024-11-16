b89b578e476b2dda84bd33fdedf087b4b7d83d30
ebd30 <108127987+ebd30@users.noreply.github.com>
2024-11-16 22:55:13 +0900

    Update 문자와 문자열 관련 함수.c

--- c/문자와 문자열 관련 함수.c
+++ c/문자와 문자열 관련 함수.c
@@ -379,4 +379,26 @@ int main()
   return 0;
 }
 
+  -그 이외의 변환 함수들
+#include <stdlib.h>
+int atoi(cosnt char * str); // 문자열의 내용을 int형으로 변환
+long atol(const char * str); // 문자열의 내용을 long형으로 변환
+double atof(const char * str); // 문자열의 내용을 double형으로 변환
+
+예제ConvStringToPrimitive.c
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  char str[20];
+  printf("정수 입력: ");
+  scanf("%s", str);
+  printf("%d \n", atoi(str));
+
+  printf("실수 입력: ");
+  scanf("%s", str);
+  printf("%g \n", atof(str));
+  return 0;
+}
+
 */
